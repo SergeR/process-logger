e5844bec4e3bbdb396acf9110a712e6c793f3b56
Serge Rodovnichenko <sergerod@gmail.com>
2018-09-10 13:28:48 +0300

    Create ProcessLogger.php

---
+++ src/ProcessLogger.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright     Copyright (c) 2018, Serge Rodovnichenko, <sergerod@gmail.com>
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace SergeR;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+
+class WaLogger extends AbstractLogger {
+}
